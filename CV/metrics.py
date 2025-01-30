7b3466612fb2a468ae98ecf856fbb894dc15ff74
chasubeen <dorocy02@ewhain.net>
2025-01-30 23:13:36 +0900

    batch 단위 변경

--- CV/metrics.py
+++ CV/metrics.py
@@ -3,43 +3,40 @@ from sklearn.metrics import precision_score, recall_score, f1_score
 
 
 def accuracy(output, target):
-    preds = torch.argmax(output, dim = -1)
-    counts = (preds == target).sum().item()
-    acc = counts / len(target)
-
+    with torch.no_grad():
+        preds = torch.argmax(output, dim = -1)
+        correct = (preds == target).sum().item()
+        acc = correct / target.size(0)
     return acc
 
 
 def top_k_error(output, target, k=5):
-    # Top-k 정확도 계산
-    top_k_preds = torch.topk(output, k, dim = -1).indices  # Top-k class의 index
-    correct = target.view(-1, 1).expand_as(top_k_preds)  # 정답을 Top-k 크기로 확장
-    counts = (top_k_preds == correct).any(dim = -1)  # Top-k 내에 정답이 있는지 확인
-    top_k_acc = counts.float().mean().item()  
+    with torch.no_grad():
+        batch_size = target.size(0)
+        _, top_pred = output.topk(k, dim = -1) # Top-k class의 index
+        correct = top_pred.eq(target.view(-1, 1).expand_as(top_pred))  # 정답을 Top-k 크기로 확장
+        correct_k = correct.any(dim = 1).float().sum().item()  # Top-k 내에 정답이 있는지 확인
+        acc_k = correct_k / batch_size
+    # Top-k Error = 1 - Top-k Accuracy    
+    return 1.0 - acc_k  
 
-    # Top-k Error = 1 - Top-k Accuracy
-    return 1.0 - top_k_acc
 
-
-## Precision: TP/(TP+FP)
 def precision(output, target, average = "weighted"):
-    preds = torch.argmax(output, dim = -1).cpu().numpy()     
-    target = target.cpu().numpy()
-    
-    return precision_score(target, preds, average = average, zero_division = 0)
+    with torch.no_grad():
+        preds = torch.argmax(output, dim = -1).cpu()
+        target = target.cpu()
+        return precision_score(target, preds, average = average, zero_division = 0)
 
 
-## Recall: TP/(TP+FN)
-def recall(output, target, average="weighted"):
-    preds = torch.argmax(output, dim = -1).cpu().numpy()
-    target = target.cpu().numpy()
-    
-    return recall_score(target, preds, average = average, zero_division = 0)
+def recall(output, target, average = "weighted"):
+    with torch.no_grad():
+        preds = torch.argmax(output, dim = -1).cpu()
+        target = target.cpu()
+        return recall_score(target, preds, average = average, zero_division = 0)
 
 
-## F1-score: 2 * {(precision * recall) / (precision + recall)}
 def f1(output, target, average = "weighted"):
-    preds = torch.argmax(output, dim = -1).cpu().numpy()
-    target = target.cpu().numpy()
-
-    return f1_score(target, preds, average = average, zero_division = 0)
+    with torch.no_grad():
+        preds = torch.argmax(output, dim = -1).cpu()
+        target = target.cpu()
+        return f1_score(target, preds, average = average, zero_division = 0)
