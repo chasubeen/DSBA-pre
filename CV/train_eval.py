bbb93804974492db84b56f9d9b12e9cf702da53d
차수빈 <subeen_cha@snu.ac.kr>
2025-02-01 02:07:02 +0900

    code 구성 변경

--- CV/train_eval.py
+++ CV/train_eval.py
@@ -1,26 +1,28 @@
 import torch
 from metrics import accuracy, top_k_error, precision, recall, f1
+from tqdm import tqdm  # 진행 상태 표시
 
 
-## 학습
+### 학습 함수
 def trainer(model, dataloader, loss, optimizer, device):
     # 학습 모드
     model.train()
 
     total_loss, total_acc = 0.0, 0.0
 
-    for x, y in dataloader:
+    for x, y in tqdm(dataloader, desc  ="Training", leave = False):
         x, y = x.to(device), y.to(device)
-        y_est = model(x)
+
+        ## Forward Pass 
+        y_est = model(x)  # Feature Vector 제외하고 출력만 사용
         cost = loss(y_est, y)
 
         total_loss += cost.item()
+        total_acc += accuracy(y_est, y)
 
         optimizer.zero_grad()
         cost.backward()
         optimizer.step()
-
-        total_acc += accuracy(y_est, y)
 
     avg_loss = total_loss / len(dataloader)
     avg_acc = total_acc / len(dataloader)
@@ -28,25 +30,28 @@ def trainer(model, dataloader, loss, optimizer, device):
     return avg_loss, avg_acc
 
 
-## 검증/평가
+
+### 평가 함수
 def evaluator(model, dataloader, loss, device):
     # 평가 모드
-    model.eval()
+    model.eval()  
 
     total_loss, total_acc = 0.0, 0.0
     total_top_k_error, total_precision, total_recall, total_f1 = 0.0, 0.0, 0.0, 0.0
 
-    with torch.no_grad():  
-        for x, y in dataloader:
+    with torch.no_grad():
+        for x, y in tqdm(dataloader, desc = "Evaluating", leave = False):
             x, y = x.to(device), y.to(device)
-             
+
+            ## Forward Pass
             y_est = model(x)
             cost = loss(y_est, y)
+
             total_loss += cost.item()
 
-            # 성능 지표 계산
+            ## 성능 평가 지표
             total_acc += accuracy(y_est, y)
-            total_top_k_error += top_k_error(y_est, y, k=5)
+            total_top_k_error += top_k_error(y_est, y, k = 5)
             total_precision += precision(y_est, y)
             total_recall += recall(y_est, y)
             total_f1 += f1(y_est, y)
